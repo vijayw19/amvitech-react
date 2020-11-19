37cf3b71dbfa926618179e53eb7f08baf1b8f6e3
vijayw19 <62520617+vijayw19@users.noreply.github.com>
2020-11-18 19:42:01 -0500

    Update image.js

--- src/image.js
+++ src/image.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 
 export default function Image() {
-    return <React.Fragment><img src={'./images/my_image.png'} className="image"/></React.Fragment>;
+    return <React.Fragment><img src={'./images/my_image.png'} alt="description of image" className="image"/></React.Fragment>;
     
 }
